4b9c0952c86816dc16d2131d3f7e2f67cf741df7
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-03-24 19:59:36 -0400

    completed the wheel functionality

--- frontend/components/Wheel.js
+++ frontend/components/Wheel.js
@@ -1,20 +1,100 @@
-import React from 'react'
+import React from "react";
+import { connect } from "react-redux";
 
-export default function Wheel(props) {
+import { moveClockwise, moveCounterClockwise } from "../state/action-creators";
+
+function Wheel(props) {
+  const { wheel, moveClockwise, moveCounterClockwise } = props;
+
+  const counterClockWiseHandler = () => {
+    switch (wheel) {
+      case 0:
+        moveCounterClockwise(5);
+        break;
+      case 1:
+        moveCounterClockwise(0);
+        break;
+      case 2:
+        moveCounterClockwise(1);
+        break;
+      case 3:
+        moveCounterClockwise(2);
+        break;
+      case 4:
+        moveCounterClockwise(3);
+        break;
+      case 5:
+        moveCounterClockwise(4);
+        break;
+      default:
+        return null;
+    }
+  };
+
+  const clockwiseHandler = () => {
+    switch (wheel) {
+      case 0:
+        moveClockwise(1);
+        break;
+      case 1:
+        moveClockwise(2);
+        break;
+      case 2:
+        moveClockwise(3);
+        break;
+      case 3:
+        moveClockwise(4);
+        break;
+      case 4:
+        moveClockwise(5);
+        break;
+      case 5:
+        moveClockwise(0);
+        break;
+      default:
+        return null;
+    }
+  };
+
   return (
     <div id="wrapper">
       <div id="wheel">
-        <div className="cog active" style={{ "--i": 0 }}>B</div>
-        <div className="cog" style={{ "--i": 1 }}></div>
-        <div className="cog" style={{ "--i": 2 }}></div>
-        <div className="cog" style={{ "--i": 3 }}></div>
-        <div className="cog" style={{ "--i": 4 }}></div>
-        <div className="cog" style={{ "--i": 5 }}></div>{/* --i is a custom CSS property, no need to touch that nor the style object */}
+        <div className={`cog ${wheel === 0 ? "active" : null}`} style={{ "--i": 0 }}>
+          {wheel === 0 ? "B" : null}
+        </div>
+        <div className={`cog ${wheel === 1 ? "active" : null}`} style={{ "--i": 1 }}>
+          {wheel === 1 ? "B" : null}
+        </div>
+        <div className={`cog ${wheel === 2 ? "active" : null}`} style={{ "--i": 2 }}>
+          {wheel === 2 ? "B" : null}
+        </div>
+        <div className={`cog ${wheel === 3 ? "active" : null}`} style={{ "--i": 3 }}>
+          {wheel === 3 ? "B" : null}
+        </div>
+        <div className={`cog ${wheel === 4 ? "active" : null}`} style={{ "--i": 4 }}>
+          {wheel === 4 ? "B" : null}
+        </div>
+        <div className={`cog ${wheel === 5 ? "active" : null}`} style={{ "--i": 5 }}>
+          {wheel === 5 ? "B" : null}
+        </div>
+        {/* --i is a custom CSS property, no need to touch that nor the style object */}
       </div>
       <div id="keypad">
-        <button id="counterClockwiseBtn" >Counter clockwise</button>
-        <button id="clockwiseBtn">Clockwise</button>
+        <button id="counterClockwiseBtn" onClick={counterClockWiseHandler}>
+          Counter clockwise
+        </button>
+        <button id="clockwiseBtn" onClick={clockwiseHandler}>
+          Clockwise
+        </button>
       </div>
     </div>
-  )
+  );
 }
+
+const mapStateToProps = (state) => {
+  return {
+    wheel: state.wheel,
+  };
+};
+
+export default connect(mapStateToProps, { moveClockwise, moveCounterClockwise })(Wheel);
--- frontend/state/action-creators.js
+++ frontend/state/action-creators.js
@@ -1,17 +1,25 @@
 // ❗ You don't need to add extra action creators to achieve MVP
-export function moveClockwise() { }
 
-export function moveCounterClockwise() { }
+import { MOVE_CLOCKWISE } from "./action-types";
+import { MOVE_COUNTERCLOCKWISE } from "./action-types";
 
-export function selectAnswer() { }
+export function moveClockwise(nextId) {
+  return { type: MOVE_CLOCKWISE, payload: nextId };
+}
 
-export function setMessage() { }
+export function moveCounterClockwise(nextId) {
+  return { type: MOVE_COUNTERCLOCKWISE, payload: nextId };
+}
 
-export function setQuiz() { }
+export function selectAnswer() {}
 
-export function inputChange() { }
+export function setMessage() {}
 
-export function resetForm() { }
+export function setQuiz() {}
+
+export function inputChange() {}
+
+export function resetForm() {}
 
 // ❗ Async action creators
 export function fetchQuiz() {
@@ -19,7 +27,7 @@ export function fetchQuiz() {
     // First, dispatch an action to reset the quiz state (so the "Loading next quiz..." message can display)
     // On successful GET:
     // - Dispatch an action to send the obtained quiz to its state
-  }
+  };
 }
 export function postAnswer() {
   return function (dispatch) {
@@ -27,13 +35,13 @@ export function postAnswer() {
     // - Dispatch an action to reset the selected answer state
     // - Dispatch an action to set the server message to state
     // - Dispatch the fetching of the next quiz
-  }
+  };
 }
 export function postQuiz() {
   return function (dispatch) {
     // On successful POST:
     // - Dispatch the correct message to the the appropriate state
     // - Dispatch the resetting of the form
-  }
+  };
 }
 // ❗ On promise rejections, use log statements or breakpoints, and put an appropriate error message in state
